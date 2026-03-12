836387129f9a80c1fb8e0c693ff0f522d6d38e31
Allan Matano <allan.matano@oxygene.africa>
2026-03-12 21:51:34 +0300

    chore: remove ngrok allowedHosts from vite config (now on AWS EC2)

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ['dbc9-41-90-109-175.ngrok-free.app'],
-  },
 })
